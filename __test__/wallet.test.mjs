fa6ecd93884aa6ad160e64cfd289346354b20611
TenTimothy <saba@wmooragency.co>
2024-08-08 12:36:05 +0200

    Wallet - in progress

---
+++ __test__/wallet.test.mjs
@@ -0,0 +1,20 @@
+import { it, describe, expect, beforeEach } from 'vitest';
+import Wallet from '../models/Wallet.mjs';
+
+describe('Wallet', ()=> {
+    let Wallet;
+
+    beforeEach(() =>{
+        wallet = new Wallet();
+    
+    });
+
+    describe('properties', () => {
+        it('should have a property named balance', () => {
+            expect(wallet).toHaveProperty('balance');
+        });
+        it('should have a propert named publicKey', () => {
+            expect(wallet).toHAveProperty('publicKey');
+        });
+    });
+});
