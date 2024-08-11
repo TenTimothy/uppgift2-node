e3b62cb896be0e61f4798ae2d58ca4fc00861215
TenTimothy <saba@wmooragency.co>
2024-08-12 00:48:59 +0200

    transaction TDD

--- __test__/transaction.test.mjs
+++ __test__/transaction.test.mjs
@@ -1,7 +1,7 @@
-import { it, describe, expect, beforeEach} from vitest;
+import { it, describe, expect, beforeEach} from 'vitest';
 import Wallet from "../models/Wallet.mjs";
 import { verifySignature } from "../utils/crypto-lib.mjs";
-import Transaction from '..models/Transaction.mjs';
+import Transaction from '../models/Transaction.mjs';
 
 describe('Transaction', () => {
     let transaction, sender, recipient, amount;
@@ -19,14 +19,24 @@ describe('Transaction', () => {
             expect(transaction).toHaveProperty('id');
         });
 
-        
         it('should have a property named outputMap', () => {
             expect(transaction).toHaveProperty('outputMap');
         });
 
-
         it('should have a property named inputMap', () => {
             expect(transaction).toHaveProperty('inputMap');
+        });
+    });
+
+    describe('outputMap()', () => {
+        it('should display the recipient\'s balance', () => {
+            expect(transaction.outputMap[recipient]).toEqual(amount);
+        });
+
+        it('should display the sender\'s balance', () => { 
+            expect(transaction.outputMap[sender.publicKey]).toEqual(
+                sender.balance - amount
+            );
         });
     });
 });
--- models/Transaction.mjs
+++ models/Transaction.mjs
@@ -1,8 +1,22 @@
+import {v4 as uuidv4} from 'uuid';
+
 export default class Transaction {
     constructor({sender, recipient, amount}) {
-        this.id = 1;
-        this.outputMap = {}
-        this.inputMap = {}
+        this.id = uuidv4().replaceAll('-', '');
+        this.outputMap = this.createOutputMap({sender, recipient, amount});
+        this.inputMap = this.createInputMap({sender, outputMap: this.outputMap});
+    }
+
+    createOutputMap({sender, recipient, amount}) { 
+        const outputMap = {};
+
+        outputMap[recipient] = amount;
+        outputMap[sender.publicKey] = sender.balance - amount;
+
+        return outputMap;
+    }
+
+    createInputMap({sender, outputMap}) { 
+        // Implementera logiken för inputMap här eller ta bort metoden om den inte behövs.
     }
 }
-
