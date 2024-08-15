47253438e4da2e2498d114c6ac8b2babe0df0330
TenTimothy <saba@wmooragency.co>
2024-08-15 23:10:44 +0200

    transaction controller

---
+++ controllers/transactionController.mjs
@@ -0,0 +1,15 @@
+import Wallet from '../models/Wallet.mjs';
+import Transaction from '../models/Transaction.mjs';
+
+export const createTransaction = (req, res, next) => {
+    try {
+        const { recipient, amount } = req.body;
+        const sender = new Wallet(); 
+        const transaction = sender.createTransaction({ recipient, amount });
+
+
+        res.status(201).json({ success: true, data: transaction });
+    } catch (error) {
+        next(error);
+    }
+};
