1481ff32fc33cd5997b8e759c4d98a531db694d4
TenTimothy <saba@wmooragency.co>
2024-08-18 00:45:48 +0200

    Added to errorHandler

--- server/middlewares/errorHandler.mjs
+++ server/middlewares/errorHandler.mjs
@@ -1,7 +1,27 @@
+import ErrorResponse from "../utils/ErrorResponse.mjs";
+
 export const errorHandler = (err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
+    console.log(err);
+
+    let error = { ...err };
+    error.message = err.message;
+
+    // Kontrollera för MongoDB duplikatnyckelfel
+    if (err.code === 11000) {
+        const message = `Resursen finns redan`;
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Kontrollera för Mongoose valideringsfel
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message).join(', ');
+        error = new ErrorResponse(`Information saknas: ${message}`, 400);
+    }
+
+    // Standardiserat felmeddelande
+    res.status(error.statusCode || 500).json({
         success: false,
-        statusCode: err.statusCode || 500,
-        error: err.message || 'Server Error'
+        statusCode: error.statusCode || 500,
+        error: error.message || 'Server Error'
     });
 };
