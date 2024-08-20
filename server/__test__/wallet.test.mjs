493f1b2abb1b9dad3ec2f5cc9bdbce72efcd9ce6
TenTimothy <saba@wmooragency.co>
2024-08-20 17:33:34 +0200

    TDD wallet

--- server/__test__/wallet.test.mjs
+++ server/__test__/wallet.test.mjs
@@ -1,6 +1,9 @@
-import { it, describe, expect, beforeEach } from 'vitest';
+import { it, describe, expect, beforeEach, vi } from 'vitest';
 import Wallet from '../models/Wallet.mjs';
 import { verifySignature } from '../utils/crypto-lib.mjs';
+import Transaction from '../models/Transaction.mjs';
+import Blockchain from '../models/Blockchain.mjs';
+import { INITIAL_BALANCE } from '../config/settings.mjs';
 
 describe('Wallet', () => {
     let wallet;
@@ -40,6 +43,25 @@ describe('Wallet', () => {
                     signature: new Wallet().sign(data),
                 })
             ).toBe(false);
+        });
+    });
+
+    describe('Calculate the balance', () => {
+        let blockchain;
+
+        beforeEach(() => {
+            blockchain = new Blockchain();
+        });
+
+        describe('and there is no output for the wallet', () => {
+            it('should return the initial balance (starting balance)', () => {
+                expect(
+                    Wallet.calculateBalance({
+                        chain: blockchain.chain, 
+                        address: wallet.publicKey 
+                    })
+                ).toEqual(INITIAL_BALANCE);
+            });
         });
     });
 });
