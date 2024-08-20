a2284865aa29ca19d4198aee16c809650a3cd809
TenTimothy <saba@wmooragency.co>
2024-08-20 17:12:16 +0200

    Changes to homePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,26 +4,36 @@ import LogoutButton from '../components/LogoutButton';
 
 const HomePage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '-90px' }}>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Welcome to Sonic-Chain</h1>
-      <nav style={{ marginTop: '20px' }}>
+      <nav style={navStyle}>
         <Link to="/send-transaction" style={linkStyle}>Send Transaction</Link>
+        <Link to="/transaction-pool" style={linkStyle}>Transaction Pool</Link>
         <Link to="/transaction-history" style={linkStyle}>Transaction History</Link>
-        <Link to="/transaction-pool" style={linkStyle}>Transaction Pool</Link> 
       </nav>
       <LogoutButton />
     </div>
   );
 };
 
+const navStyle = {
+  display: 'flex',
+  flexDirection: 'column', 
+  alignItems: 'center',  
+  gap: '15px', 
+  marginTop: '20px',
+};
+
 const linkStyle = {
-  display: 'inline-block',
-  margin: '0 15px',
+  display: 'block',
   padding: '10px 20px',
   backgroundColor: 'white',
   color: 'black',
   borderRadius: '4px',
   textDecoration: 'none',
+  width: '100%', 
+  maxWidth: '200px', 
+  textAlign: 'center',
 };
 
 export default HomePage;
